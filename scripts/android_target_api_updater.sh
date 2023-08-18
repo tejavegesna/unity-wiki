eb53d7b8649f47a37af4b8f93ad152f3152bda66
Teja Vegesna <9069289+tejavegesna@users.noreply.github.com>
2023-08-18 11:56:08 -0400

    Create android_target_api_updater.sh

---
+++ scripts/android_target_api_updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the required arguments were provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <unity_version> <android_version>"
+    exit 1
+fi
+
+UNITY_VERSION=$1
+ANDROID_VERSION=$2
+
+# Change directory to the location of the sdkmanager
+cd /Applications/Unity/Hub/Editor/$UNITY_VERSION/PlaybackEngines/AndroidPlayer/SDK/cmdline-tools/2.1/bin
+
+# Check if the directory exists
+if [ ! -d "$(pwd)" ]; then
+    echo "Error: Directory not found. Please check the Unity version."
+    exit 1
+fi
+
+# Install the desired Android platform
+./sdkmanager "platforms;android-$ANDROID_VERSION"
